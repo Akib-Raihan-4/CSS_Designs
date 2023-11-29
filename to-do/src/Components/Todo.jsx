ed62e49ceb8d9a4e2e1a936f771d814eaac44911
Akib-Raihan-4 <raihanakib062@gmail.com>
2023-11-30 04:04:42 +0600

    Todo and todo items

--- to-do/src/Components/Todo.jsx
+++ to-do/src/Components/Todo.jsx
@@ -1,18 +1,34 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import './CSS/Todo.css'
+import TodoItems from './TodoItems';
 
+let count = 0;
 const Todo = () => {
+  const [todos, setTodos] = useState([]);
+  const inputRef = useRef(null);
+
+  const add = () => {
+    setTodos([...todos, {no: count++, text: inputRef.current.value, display:""}]);
+    inputRef.current.value = "";
+  }
+
+  useEffect(()=>{
+    console.log(todos)
+  }, [todos])
+
   return (
     <div className='todo'>
         <div className="todo-header">
             To-Do List
         </div>
         <div className="todo-add">
-            <input type="text" placeholder='Add your task' className='todo-input'/>
-            <div className="todo-add-btn">ADD</div>
+            <input ref={inputRef} type="text" placeholder='Add your task' className='todo-input'/>
+            <div onClick={()=>{add()}} className="todo-add-btn">ADD</div>
         </div>
         <div className="todo-list">
-            
+            {todos.map((item,index)=>{
+              return <TodoItems key={index} no={item.no} display={item.display} text ={item.text}/>
+            })}
         </div>
     </div>
   )
--- to-do/src/Components/TodoItems.jsx
+++ to-do/src/Components/TodoItems.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
-import './CSS/TodoItems.css'
-
+import './CSS/Todoitems.css'
+import tick from './Assets/tick.png'
+import not_tick from './Assets/not_tick.png'
+import cross from './Assets/cross.png'
 
 const TodoItems = () => {
   return (
-    <div>TodoItems</div>
+    <div className='todoitems'>
+      <div className="todoitems-container">
+        <img src={not_tick} alt="" />
+        <img src={tick} alt="" />
+        <div className="todoitems-text">
+        </div>
+      </div>
+      <img src={cross} alt="" />
+    </div>
   )
 }
 
